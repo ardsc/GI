4cf2e36453a8b4755cd56da6997e8bfb6f837889
root <syahidnurrohim@gmail.com>
2020-10-09 22:28:09 +0700

    update facade

--- Foundation/Facade.php
+++ Foundation/Facade.php
@@ -10,20 +10,39 @@ abstract class Facade
 
     protected static $app;
 
-    public static function getPackageName()
+    /**
+     * Override service name
+     * @throws Exception
+     */
+    protected static function setBindIdentity()
     {
-        throw new RuntimeException('Unable to initialzie package object');
+        throw new Exception('Unable to initialzie package object');
     }
 
+    /**
+     * Register service to application registry
+     *
+     * @param $name
+     * @return array|mixed
+     */
     public static function resolveBinding($name)
     {
-        self::$app = app('app');
+        self::$app = app();
+        self::$app->registerService($name, get_called_class());
         return self::$app->resolveBinding($name);
     }
 
+    /**
+     * Call Static magic method for dynamically access non static method
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws Exception
+     */
     public static function __callStatic($name, $arguments)
     {
-        $instance = self::resolveBinding(self::getPackageName());
+        $instance = self::resolveBinding(static::setBindIdentity());
 
         if (! is_object($instance)) {
             throw new Exception('Package is not a concrete object');
